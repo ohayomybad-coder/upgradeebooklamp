2dc81bb5c71656f195314015761111a1d21925ec
ohayomybad-coder <ohayomybad@gmail.com>
2026-05-14 08:47:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,93 @@
-// script.js
+// SEARCH + FILTER SYSTEM
 
-console.log("Noctara Loaded Successfully");
+const searchBar = document.querySelector(".search-bar");
+
+const genreButtons =
+  document.querySelectorAll(".genre-btn");
+
+const bookLinks =
+  document.querySelectorAll(".book-link");
+
+// SEARCH
+
+searchBar.addEventListener("input", () => {
+
+  const searchValue =
+    searchBar.value.toLowerCase();
+
+  bookLinks.forEach((book) => {
+
+    const title =
+      book.querySelector("h3")
+      .textContent
+      .toLowerCase();
+
+    if(title.includes(searchValue)){
+
+      book.style.display = "block";
+
+    } else {
+
+      book.style.display = "none";
+
+    }
+
+  });
+
+});
+
+// GENRE FILTER
+
+genreButtons.forEach((button) => {
+
+  button.addEventListener("click", () => {
+
+    genreButtons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+
+    const genre =
+      button.textContent.toLowerCase();
+
+    bookLinks.forEach((book) => {
+
+      const title =
+        book.querySelector("h3")
+        .textContent
+        .toLowerCase();
+
+      if(
+        genre === "all" ||
+
+        (genre === "fantasy" &&
+          title.includes("lantern")) ||
+
+        (genre === "horror" &&
+          title.includes("rain")) ||
+
+        (genre === "philosophy" &&
+          title.includes("fragments")) ||
+
+        (genre === "romance" &&
+          title.includes("fragments")) ||
+
+        (genre === "sci-fi" &&
+          title.includes("lantern"))
+
+      ){
+
+        book.style.display = "block";
+
+      } else {
+
+        book.style.display = "none";
+
+      }
+
+    });
+
+  });
+
+});
